e6298f5fba4554672f3d842f223bc15a1ec550d7
Bryan Vinicius Gomes <gomesbryan754@gmail.com>
2026-06-21 23:13:05 -0300

    retornos de erros

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -4,4 +4,16 @@ export const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_BASE_URL ||
     "http://localhost:3000",
+    
 });
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.error("API ERROR:", {
+      status: error.response?.status,
+      data: error.response?.data,
+    });
+
+    return Promise.reject(error);
+  }
+);
